febca8ac51bf278d1b1832b06afd64d02686c003
philipbrown <phil@ipbrown.com>
2023-03-14 21:28:28 +0000

    Flesh out haystack module

--- lib/haystack.ex
+++ lib/haystack.ex
@@ -2,4 +2,47 @@ defmodule Haystack do
   @moduledoc """
   Haystack is a simple, extendable search engine written in Elixir.
   """
+
+  alias Haystack.Index
+
+  # Types
+
+  @type t :: %__MODULE__{
+          indexes: %{atom => Index.t()}
+        }
+
+  @enforce_keys ~w{indexes}a
+
+  defstruct @enforce_keys
+
+  # Public
+
+  @doc """
+  Create a new Haystack.
+
+  ## Examples
+
+    iex> Haystack.new()
+
+  """
+  @spec new(Keyword.t()) :: t
+  def new(_opts \\ []),
+    do: struct(__MODULE__, indexes: %{})
+
+  @doc """
+  Interact with the given index.
+
+  ## Examples
+
+    iex> haystack = Haystack.new()
+    iex> Haystack.index(haystack, :animals, &Function.identity/1)
+
+  """
+  @spec index(t, atom, (Index.t() -> term)) :: t | term
+  def index(hs, name, f) do
+    case f.(Map.get(hs.indexes, name, Index.new(name))) do
+      %Index{} = index -> %{hs | indexes: Map.put(hs.indexes, name, index)}
+      response -> response
+    end
+  end
 end
--- test/haystack_test.exs
+++ test/haystack_test.exs
@@ -1,5 +1,35 @@
 defmodule HaystackTest do
   use ExUnit.Case, async: true
 
+  import Haystack.Fixture
+
+  alias Haystack.Index
+
   doctest Haystack
+
+  describe "new/1" do
+    test "should create haystack" do
+      assert %Haystack{} = Haystack.new()
+    end
+  end
+
+  describe "index/3" do
+    setup do
+      fixture(:animals)
+    end
+
+    test "should use index", %{data: data} do
+      results =
+        Haystack.index(Haystack.new(), :animals, fn index ->
+          index
+          |> Index.ref(Index.Field.term("id"))
+          |> Index.field(Index.Field.new("name"))
+          |> Index.field(Index.Field.new("description"))
+          |> Index.add(data)
+          |> Index.search("panda")
+        end)
+
+      assert Enum.count(results) == 2
+    end
+  end
 end
